f3cc20df29b394689e248a62fa5c83cdb00cf83a
Alexay Mehra <alexay.nmehra@gmail.com>
2025-12-21 02:10:32 -0500

    Improved makefile & cpu testbench

--- test/cpuTB.v
+++ test/cpuTB.v
@@ -10,45 +10,54 @@ module cpu_8b_tb;
         .halt(halt)
     );
 
-    // VCD dump
     initial begin
         $dumpfile("cpuTB.vcd");
         $dumpvars(0, cpu_8b_tb);
     end
 
-    // Clock generator
     initial begin
         clk = 0;
         forever #5 clk = ~clk;  // 10ns clock period
     end
 
-    // Reset and simulation control
+    // Reset
     initial begin
         rst = 1;
-        #20;       // assert reset for 1 full clock period
+        #20;       
         rst = 0;
 
-        // Monitor IR and PC every clock
-        forever @(posedge clk) begin
-            $display("Time %0t: IR = %b, PC = %b, Halt = %b", $time,
-                     cpu_unit.instruction_register.q,
-                     cpu_unit.program_counter.q,
-                     halt);
-        end
-    end
-
-    // Stop simulation when halted or timeout
-    initial begin
         fork
             begin
-                wait(halt);
-                #10; $finish;
+                wait(halt); #20; $finish;
             end
             begin
                 #1000;  // timeout
                 $display("Simulation timeout"); $finish;
             end
         join
+    end
+
+    always @(posedge clk) begin
+        $timeformat(-9, 0, " ns", 10);
+        $display("Time %0t:", $time);
+        $display("IR = %b,  PC = %b,  State = %b",
+            cpu_unit.instr,
+            cpu_unit.pc,
+            cpu_unit.state, 
+        );
+        $display("A_REG = %b,  B_REG = %b",
+            cpu_unit.a_reg_o,
+            cpu_unit.b_reg_o
+        );
+        $display("MEM_ADDR = %b,  MEM_DATA = %b",
+            cpu_unit.mem_addr,
+            cpu_unit.mem_d_o
+        );
+        $display("ALU_RES = %b,  ZF = %b",
+            cpu_unit.alu_res,
+            cpu_unit.zf
+        );
+        $display("Reset = %b,  Halt = %b\n", rst, halt);
     end
 
 endmodule
